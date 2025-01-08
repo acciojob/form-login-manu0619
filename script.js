c3e1b5b7c1e3f4f8183822c990e44cf9c6295b26
manu0619 <125184498+manu0619@users.noreply.github.com>
2025-01-08 18:54:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+	a=document.getElementByName('fname');
+	b=document.getElementByName('lname');
+	document.querySelector("input").addEventListener('click',()=>{
+		alert(`${a.value} ${b.value}`);
+	})
 
 }
